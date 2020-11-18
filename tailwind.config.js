dd2d598684a6e63785616fdb53d502ddb2fa251b
ndom91 <yo@ndo.dev>
2020-11-18 14:52:11 +0100

    update: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+    standardFontWeights: true,
   },
   purge: ["./src/**/*.js", "./src/**/*.jsx"],
   theme: {
